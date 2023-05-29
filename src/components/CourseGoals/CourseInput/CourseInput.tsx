b953cdb7c7a8f23b63641ec8c4f0e9f89d299402
saidmtanzania <saidmtanzania@gmail.com>
2023-05-29 13:26:12 +0300

    feat: validating user input

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -6,21 +6,34 @@ import "./CourseInput.css";
 
 function CourseInput(props: any) {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, checkValid] = useState(true);
 
   const goalInputChangeHandler = (event: any) => {
+    if (event.target.value.trim().length > 0) checkValid(true);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event: any) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      checkValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "#ccc",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
