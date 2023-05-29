d5736e81b66bf3d8a27efbe0764fc433b3db29e1
saidmtanzania <saidmtanzania@gmail.com>
2023-05-29 11:13:15 +0300

    feat: setup new components and its styles

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import "./Button.css";
+
+function Button(props: any) {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
