78f3deb386141a72ba43d9b3130d61d04eba87c6
saidmtanzania <saidmtanzania@gmail.com>
2023-05-29 11:12:30 +0300

    fix: fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
-import { useState } from "react";
-
-const expensesData = [
-  {
-    id: "e1",
-    title: "Playful Joy",
-    amount: 450000,
-    date: new Date(2021, 3, 25),
-  },
-  {
-    id: "e2",
-    title: "Boom SMS",
-    amount: 505750,
-    date: new Date(2022, 5, 4),
-  },
-  {
-    id: "e3",
-    title: "Bolt",
-    amount: 250000,
-    date: new Date(2023, 7, 3),
-  },
-];
 
 function App() {
-  const [expenses, setExpenses] = useState(expensesData);
-
-  const addExpenseHandler = (expense: any) => {
-    setExpenses((prevExpenses: any) => [expense, ...prevExpenses]);
-  };
-
-  return (
-    <>
-      <NewExpense addedExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/UI/Card.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import "./Card.css";
-function Card(props: any) {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-}
-
-export default Card;
