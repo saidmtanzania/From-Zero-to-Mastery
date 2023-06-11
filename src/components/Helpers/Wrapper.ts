d90ed332fa27af26a22e44d4f98ccfeabbeb2ccf
saidmtanzania <saidmtanzania@gmail.com>
2023-06-11 19:59:56 +0300

    feat: setup error  modal alignment also setup wrapper around the components to avoid div soups

---
+++ src/components/Helpers/Wrapper.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const Wrapper = (props: any) => {
+  return props.children;
+};
+
+export default Wrapper;
--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
@@ -52,7 +53,7 @@ const AddUser = (props: {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -79,7 +80,7 @@ const AddUser = (props: {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
